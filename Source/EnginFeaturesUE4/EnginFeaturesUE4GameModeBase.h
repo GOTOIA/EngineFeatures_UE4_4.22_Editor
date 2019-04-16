241532692ef6d0a139528fc23f8c1857d2fc6d39
nglconsulting <nico.nglconsulting@gmail.com>
2019-04-16 13:12:01 +0200

    Firts Commit : Level Building fichier Lvl1 Creation du terrain Sculptage du Terrain Mise en forme (texture, matières) Terrain

---
+++ Source/EnginFeaturesUE4/EnginFeaturesUE4GameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EnginFeaturesUE4GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ENGINFEATURESUE4_API AEnginFeaturesUE4GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
