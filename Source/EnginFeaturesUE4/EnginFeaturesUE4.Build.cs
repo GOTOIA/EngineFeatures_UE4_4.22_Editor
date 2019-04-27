dd314f91d2e58654216680e9b1c32fd83b76c764
nglconsulting <nico.nglconsulting@gmail.com>
2019-04-27 12:02:26 +0200

    Modification Level Modification des collisions sur les meshs du level(Maisons...), ajout d'une collision complexe Ajout  features ThirdPerson et First Person Fix bug C++ features TPS et FPS

--- Source/EnginFeaturesUE4/EnginFeaturesUE4.Build.cs
+++ Source/EnginFeaturesUE4/EnginFeaturesUE4.Build.cs
@@ -8,7 +8,7 @@ public class EnginFeaturesUE4 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" , "HeadMountedDisplay" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
